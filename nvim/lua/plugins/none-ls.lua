b6c9e2e12a273efcc87f387e58c278e4fa22bb3e
Siddhartha Shukla <112815574+S1ddharthaShukla@users.noreply.github.com>
2025-04-10 18:42:14 +0530

    Update none-ls.lua

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,20 +3,21 @@ return {
   dependencies = {
     "nvimtools/none-ls-extras.nvim",
   },
-	config = function()
-		local null_ls = require("null-ls")
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
+    null_ls.setup({
+      sources = {
+	null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+	null_ls.builtins.formatting.clang_format,
         null_ls.builtins.completion.spell,
         require("none-ls.diagnostics.eslint_d")
-			},
-		})
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
